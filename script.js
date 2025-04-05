57a2dc07fe6c12587313a0d393fdf5cc0fa0af4d
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2025-04-05 10:49:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+function findval(arr){
+	let elem  = arr[0];
+	let count = 1
+	for (let i = 1; i <arr.length;i++){
+
+		if(arr[i] == elem){
+			count++;
+		}else{
+			count--;
+			if(count == 0){
+				elem = arr[i];
+				count = 1;
+			}
+		}
+	}
+	return elem;
+}
