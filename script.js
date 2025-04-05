b23df97a63d0d3d1713761d68ca497d8a722dab1
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2025-04-05 10:53:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-//your code here
-function findval(arr){
-	let elem  = arr[0];
-	let count = 1
-	for (let i = 1; i <arr.length;i++){
+function findval(arr) {
+    let elem = arr[0], count = 1;
 
-		if(arr[i] == elem){
-			count++;
-		}else{
-			count--;
-			if(count == 0){
-				elem = arr[i];
-				count = 1;
-			}
-		}
-	}
-	return elem;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] === elem) {
+            count++;
+        } else {
+            count--;
+            if (count === 0) {
+                elem = arr[i];
+                count = 1;
+            }
+        }
+    }
+
+    // Optional verification step:
+    let freq = arr.filter(x => x === elem).length;
+    if (freq > Math.floor(arr.length / 2)) {
+        return elem;
+    } else {
+        return null; // No majority element
+    }
 }
